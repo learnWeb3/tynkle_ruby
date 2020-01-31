1588d7c2bc54987416b7e00f8686d9947aad7b04
antoinehadrienleguillou <antoinehadrienleguillou@outlook.com>
2020-01-31 13:46:52 +0100

    syntax error on script change color

--- app/assets/javascripts/changeColorButton.js
+++ app/assets/javascripts/changeColorButton.js
@@ -1,13 +1,13 @@
 $(window).scroll(function() {
-            var scroll = $(window).scrollTop();
-            var docHeigth = $(document).height()
+    var scroll = $(window).scrollTop();
+    var docHeigth = $(document).height()
 
-            if (scroll >= (docHeigth / 8) {
-                    $("#problem-button").removeClass("btn-primary");
-                    $("#problem-button").addClass("btn-success");
+    if (scroll >= (docHeigth / 8)) {
+        $("#problem-button").removeClass("btn-primary");
+        $("#problem-button").addClass("btn-success");
 
-                } else if (scroll == 0) {
-                    $("#problem-button").removeClass("btn-success");
-                    $("#problem-button").addClass("btn-primary");
-                }
-            });
+    } else if (scroll == 0) {
+        $("#problem-button").removeClass("btn-success");
+        $("#problem-button").addClass("btn-primary");
+    }
+});
