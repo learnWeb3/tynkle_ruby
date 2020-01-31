6f2e5a7a75ff2c158508d20b61be4caa76a0d343
antoinehadrienleguillou <antoinehadrienleguillou@outlook.com>
2020-01-31 13:30:38 +0100

    adding jquery scrolltop event to change navbar problem button color on scroll down of the page

---
+++ app/assets/javascripts/changeColorButton.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            var docHeigth = $(document).height()
+
+            if (scroll >= (docHeigth / 8) {
+                    $("#problem-button").removeClass("btn-primary");
+                    $("#problem-button").addClass("btn-success");
+
+                } else if (scroll == 0) {
+                    $("#problem-button").removeClass("btn-success");
+                    $("#problem-button").addClass("btn-primary");
+                }
+            });
