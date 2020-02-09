cc3e2b02dfc228fb452be717fa236ff0ddebe09c
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-09 18:22:04 +0100

    ok

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -15,7 +15,8 @@ class MissionsController < ApplicationController
         new_mission = Mission.new(user:current_user,title:mission_title, description:mission_description,price:mission_price, device_category:mission_device_category,categorytag:mission_category_tag, address:mission_address, image_one:mission_image_one, image_two:mission_image_two, image_three:mission_image_three)
 
         if new_mission.save
-          redirect_to "/servicesurvey/select_helper?#{new_mission.id}"
+          session[:mission] = new_mission.id
+          redirect_to "/servicesurvey/select_helper"
         end
 
 
