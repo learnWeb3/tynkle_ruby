061e609f629db2da774abf1736b9fc4603e0b3e8
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-10 12:46:20 +0100

    ok

--- app/assets/javascripts/changeColorButton.js
+++ app/assets/javascripts/changeColorButton.js
@@ -6,14 +6,33 @@ $(window).scroll(function() {
         $("#problem-button").addClass("btn-success");
         $("#navbar").removeClass("bg-transparent navbar-dark");
         $("#navbar").addClass("bg-white navbar-white");
-        document.getElementById("logo-brand").src = 'http://localhost:3000/assets/logo/brand.png'
+        document.getElementById("logo-brand").src = 'http://localhost:3000/assets/logo/brand.png';
+        $("#navbar-link-1").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-2").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-3").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-3").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-5").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-6").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
+        $("#navbar-link-4").removeClass("hover-weigth-underline-white").addClass("hover-weigth-underline-dark")
 
+
+
+         
+     
 
     } else if (scroll < docHeigth / 9) {
         $("#problem-button").removeClass("btn-success");
         $("#navbar").addClass("bg-transparent");
         $("#navbar").removeClass("bg-white navbar-white");
         $("#navbar").addClass("bg-transparent navbar-dark");
-        document.getElementById("logo-brand").src = 'http://localhost:3000/assets/logo/brand-light.png'
+        document.getElementById("logo-brand").src = 'http://localhost:3000/assets/logo/brand-light.png';
+        $("#navbar-link-1").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        $("#navbar-link-2").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        $("#navbar-link-3").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        $("#navbar-link-4").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        $("#navbar-link-5").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        $("#navbar-link-6").removeClass("hover-weigth-underline-dark").addClass("hover-weigth-underline-white")
+        
+
     }
 });
