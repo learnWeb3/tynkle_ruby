467cd3313349f9db182b011fa11577b91be6d604
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-13 17:25:52 +0100

    update with photos

--- app/controllers/useroutsidedevise_controller.rb
+++ app/controllers/useroutsidedevise_controller.rb
@@ -2,20 +2,36 @@ class UseroutsidedeviseController < ApplicationController
 
     def update
 
+
         user = User.find(params["id"].to_i)
 
-        set_first_name = params["user"]["first_name"]
-        set_last_name = params["user"]["last_name"]
-        set_date_of_birth = params["user"]["date_of_birth"]
-        set_helper = params["user"]["service_provider"]
-        set_address = params["user"]["address"]
+        if params["user"].present?
+            if params["user"]["first_name"].present?
+                set_first_name = params["user"]["first_name"]
+            end
+            if params["user"]["last_name"].present?
+                set_last_name = params["user"]["last_name"]
+            end
+            if params["user"]["date_of_birth"].present?
+                set_date_of_birth = params["user"]["date_of_birth"]
+            end
+            if params["user"]["service_provider"].present?
+                set_helper = params["user"]["service_provider"]
+            end
+            if params["user"]["address"].present?
+                set_address = params["user"]["address"]
+            end
+            if params["user"]["avatar"].present?
+                set_avatar = params["user"]["avatar"]
+            end
+        end
 
 
 
         set_helper_params_to_user_model_attribute(set_helper)
 
 
-        check_updated_attributes(user, set_date_of_birth, set_first_name, set_last_name, set_helper, set_address)
+        check_updated_attributes(user, set_date_of_birth, set_first_name, set_last_name, set_helper, set_address, set_avatar)
 
        
 
@@ -32,7 +48,7 @@ end
 
 private 
 
-def check_updated_attributes(user,set_date_of_birth,set_first_name,set_last_name,set_helper, set_address)
+def check_updated_attributes(user,set_date_of_birth,set_first_name,set_last_name,set_helper, set_address, set_avatar)
 
     if set_date_of_birth != nil 
         set_date_of_birth = set_date_of_birth.to_i
@@ -51,6 +67,9 @@ def check_updated_attributes(user,set_date_of_birth,set_first_name,set_last_name
     end
     if set_address != nil && set_address != user.address
         user.address = set_address
+    end
+    if set_avatar != nil && set_avatar != user.avatar
+        user.avatar = set_avatar
     end
 
 end
