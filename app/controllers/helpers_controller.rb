6188926b320b707cf688c307d445475c079f0d78
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-14 18:09:35 +0100

    skills resume

--- app/controllers/helpers_controller.rb
+++ app/controllers/helpers_controller.rb
@@ -6,6 +6,7 @@ class HelpersController < ApplicationController
 
     def show
         @user = User.find(params["id"].to_i)
+        @user_skills = LinkSkillToUser.where(user:@user, acquired:true)
     end
 
 end
