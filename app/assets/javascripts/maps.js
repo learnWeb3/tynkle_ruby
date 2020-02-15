8db4573397855bb3ab96df4b5ea02bb99a22f446
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-15 10:47:13 +0100

    ok

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,8 +1,21 @@
+var userName = 'Ma position'
 
-function initMap(latitude,longitude, targetedDiv) {
+
+function initMap(latitude,longitude, targetedDiv, userName) {
+
   map = new google.maps.Map(targetedDiv, {
   center: {lat: latitude, lng: longitude},
   zoom: 14
 });
+
+
+var userPosition = {lat: latitude, lng: longitude};
+
+var marker = new google.maps.Marker({
+  position: userPosition,
+  map: map,
+  title: userName
+});
+
 
 };
