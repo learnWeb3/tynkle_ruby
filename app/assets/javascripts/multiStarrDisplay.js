284e42063b0acfa8dfc4d50937ddf091122226f4
Antoine <antoinehadrienleguillou@outlook.com>
2020-02-25 15:58:57 +0100

    ok

--- app/assets/javascripts/multiStarrDisplay.js
+++ app/assets/javascripts/multiStarrDisplay.js
@@ -2,24 +2,30 @@
 function multiStarrDisplay() {
 
 $('.starrr-0').starrr({
-    rating: 0
+    rating: 0,
+    readOnly: true
   });
 
 $('.starrr-1').starrr({
-    rating: 1
+    rating: 1,
+    readOnly: true
   });
 
   $('.starrr-2').starrr({
-    rating: 2
+    rating: 2,
+    readOnly: true
   });
   $('.starrr-3').starrr({
-    rating: 3
+    rating: 3,
+    readOnly: true
   });
   $('.starrr-4').starrr({
-    rating: 4
+    rating: 4,
+    readOnly: true
   });
   $('.starrr-5').starrr({
-    rating: 5
+    rating: 5,
+    readOnly: true
   });
 
 };
